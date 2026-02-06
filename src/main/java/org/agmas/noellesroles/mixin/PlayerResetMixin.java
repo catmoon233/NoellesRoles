d98ec0a2f2b5627f04b290361e8baa652565822a
wifi-left <ysy1679682327@outlook.com>
2026-02-06 17:09:58 +0800

    fix

--- src/main/java/org/agmas/noellesroles/mixin/PlayerResetMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/PlayerResetMixin.java
@@ -115,6 +115,7 @@ public abstract class PlayerResetMixin {
         // 删除modifier
         WorldModifierComponent worldModifierComponent = WorldModifierComponent.KEY.get(player.level());
         worldModifierComponent.modifiers.clear();
+        worldModifierComponent.sync();
         // 清除该玩家放置的所有灾厄印记实体
         clearCalamityMarks(player);
     }
