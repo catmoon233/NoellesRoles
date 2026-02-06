5f7a58728022854f07622b2600c88292c2e2a75a
wifi-left <ysy1679682327@outlook.com>
2026-02-06 16:06:14 +0800

    configWorld no RoleComponent

--- src/main/java/org/agmas/noellesroles/ConfigWorldComponent.java
+++ src/main/java/org/agmas/noellesroles/ConfigWorldComponent.java
@@ -1,22 +1,20 @@
 package org.agmas.noellesroles;
 
-import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import org.agmas.noellesroles.config.NoellesRolesConfig;
 import org.jetbrains.annotations.NotNull;
 import org.ladysnake.cca.api.v3.component.ComponentKey;
 import org.ladysnake.cca.api.v3.component.ComponentRegistry;
-import dev.doctor4t.trainmurdermystery.api.RoleComponent;
-import org.ladysnake.cca.api.v3.component.tick.ClientTickingComponent;
+import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
+
 import org.ladysnake.cca.api.v3.component.tick.ServerTickingComponent;
 import net.minecraft.world.entity.player.Player;
 
-import java.util.UUID;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 
-public class ConfigWorldComponent implements RoleComponent, ServerTickingComponent {
+public class ConfigWorldComponent implements AutoSyncedComponent, ServerTickingComponent {
     public static final ComponentKey<ConfigWorldComponent> KEY = ComponentRegistry.getOrCreate(ResourceLocation.fromNamespaceAndPath(Noellesroles.MOD_ID, "config"), ConfigWorldComponent.class);
     public boolean insaneSeesMorphs = true;
     public boolean naturalVoodoosAllowed = false;
@@ -32,7 +30,6 @@ public class ConfigWorldComponent implements RoleComponent, ServerTickingCompone
         this.world = world;
     }
 
-    @Override
     public Player getPlayer() {
         return null;
     }
