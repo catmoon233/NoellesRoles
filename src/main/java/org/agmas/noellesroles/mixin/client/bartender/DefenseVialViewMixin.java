10b96875b6373774cf5cdead7603dc9f5ec3d75a
wifi-left <ysy1679682327@outlook.com>
2026-02-06 16:05:29 +0800

    remove unnecessary

--- src/main/java/org/agmas/noellesroles/mixin/client/bartender/DefenseVialViewMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/client/bartender/DefenseVialViewMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.UUID;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
