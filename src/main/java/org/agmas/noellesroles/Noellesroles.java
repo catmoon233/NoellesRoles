cf6d1654a106516770444070445cb8224fdfb008
wifi-left <ysy1679682327@outlook.com>
2026-02-14 15:34:03 +0800

    增强游侠

--- src/main/java/org/agmas/noellesroles/Noellesroles.java
+++ src/main/java/org/agmas/noellesroles/Noellesroles.java
@@ -672,7 +672,7 @@ public class Noellesroles implements ModInitializer {
             PoisonArrow.set(DataComponents.ITEM_NAME, Component.translatable("item.poison_arrow.name"));
             PoisonArrow.set(DataComponents.POTION_CONTENTS, new PotionContents(Potions.POISON));
             PoisonArrow.set(DataComponents.MAX_STACK_SIZE, 1);
-            shopEntries.add(new ShopEntry(PoisonArrow, 125, ShopEntry.Type.WEAPON) {
+            shopEntries.add(new ShopEntry(PoisonArrow, 100, ShopEntry.Type.WEAPON) {
                 @Override
                 public boolean onBuy(@NotNull Player player) {
                     int itemCount = 0;
