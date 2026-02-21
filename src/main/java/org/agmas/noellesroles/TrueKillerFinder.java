3bc2f2e89cd0ff8cca121eb120f328bd09770b9c
wifi-left <ysy1679682327@outlook.com>
2026-02-21 13:18:21 +0800

    some changes

--- src/main/java/org/agmas/noellesroles/TrueKillerFinder.java
+++ src/main/java/org/agmas/noellesroles/TrueKillerFinder.java
@@ -3,17 +3,17 @@ package org.agmas.noellesroles;
 import org.agmas.noellesroles.component.ConspiratorPlayerComponent;
 import org.agmas.noellesroles.role.ModRoles;
 
+import dev.doctor4t.trainmurdermystery.TMM;
 import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
 import dev.doctor4t.trainmurdermystery.cca.PlayerPoisonComponent;
 import dev.doctor4t.trainmurdermystery.event.EarlyKillPlayer;
+import dev.doctor4t.trainmurdermystery.game.GameFunctions;
 import net.minecraft.server.level.ServerPlayer;
 
 public class TrueKillerFinder {
 
     public static void registerEvents() {
         EarlyKillPlayer.FIND_KILLER_EVENT.register((victim, originalKiller, deathReason) -> {
-            if (originalKiller != null)
-                return null;
             if (!(victim instanceof ServerPlayer serverVictim))
                 return null;
             // Noellesroles.LOGGER.info("!!!");
@@ -21,9 +21,23 @@ public class TrueKillerFinder {
             var poisonerC = PlayerPoisonComponent.KEY.maybeGet(victim).orElse(null);
             if (poisonerC != null) {
                 if (poisonerC.poisoner != null) {
-                    return serverVictim.level().getPlayerByUUID(poisonerC.poisoner);
+                    var poisonerP = serverVictim.level().getPlayerByUUID(poisonerC.poisoner);
+                    if (poisonerP != null && !deathReason.getPath().equals("poison") && originalKiller != null
+                            && !poisonerC.poisoner.equals(originalKiller.getUUID())) {
+
+                        GameFunctions.killPlayer(victim, false, poisonerP, TMM.id("poison"));
+                        return null;
+                    }
+                    if (originalKiller != null)
+                        return null;
+                    return poisonerP;
                 }
             }
+
+            if (originalKiller != null)
+                return null;
+            if (gameWorldComponent.isRole(serverVictim, ModRoles.CONSPIRATOR))
+                return null;
             // 是否为阴谋家击杀
             for (var player : serverVictim.level().players()) {
                 if (gameWorldComponent.isRole(player, ModRoles.CONSPIRATOR)) {
