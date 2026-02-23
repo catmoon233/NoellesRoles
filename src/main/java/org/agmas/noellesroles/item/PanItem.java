9901926a8bca8e2b968525739f8db52d0c654791
wifi-left <ysy1679682327@outlook.com>
2026-02-23 14:45:08 +0800

    API UPDATE

--- src/main/java/org/agmas/noellesroles/item/PanItem.java
+++ src/main/java/org/agmas/noellesroles/item/PanItem.java
@@ -1,10 +1,37 @@
 // Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package org.agmas.noellesroles.item;
 
+import dev.doctor4t.trainmurdermystery.game.GameFunctions;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class PanItem extends Item {
+    public static Runnable openScreenCallback = null;
     public PanItem(Properties properties) {
         super(properties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player user, InteractionHand hand){
+        ItemStack stack = user.getItemInHand(hand);
+        
+        // 验证：玩家必须存活
+        if (!GameFunctions.isPlayerAliveAndSurvival(user)) {
+            return InteractionResultHolder.fail(stack);
+        }
+        
+        // 客户端：打开GUI
+        if (world.isClientSide()) {
+            if (openScreenCallback != null) {
+                openScreenCallback.run();
+            }
+        }
+        
+        // 返回 success 但不消耗物品，等猜测完成后再消耗
+        return InteractionResultHolder.success(stack);
+
     }
 }
