4a4bd55b2d96283a9ca522d99099282fad57f7f7
wifi-left <ysy1679682327@outlook.com>
2026-02-25 13:53:23 +0800

    1

--- src/main/java/org/agmas/noellesroles/commands/BroadcastCommand.java
+++ src/main/java/org/agmas/noellesroles/commands/BroadcastCommand.java
@@ -11,34 +11,34 @@ import net.minecraft.server.level.ServerPlayer;
 import java.util.Iterator;
 
 public class BroadcastCommand {
-    @SuppressWarnings("rawtypes")
-    public static void register() {
-        CommandRegistrationCallback.EVENT.register((commandDispatcher, registryAccess, environment) -> {
-            commandDispatcher.register((Commands.literal("broadcast")
-                    .requires((commandSourceStack) -> {
-                        return commandSourceStack.hasPermission(2);
-                    })).then(Commands.argument("targets", EntityArgument.players())
-                            .then(Commands.argument("message", ComponentArgument.textComponent(registryAccess))
-                                    .executes((commandContext) -> {
-                                        int i = 0;
+  @SuppressWarnings("rawtypes")
+  public static void register() {
+    CommandRegistrationCallback.EVENT.register((commandDispatcher, registryAccess, environment) -> {
+      commandDispatcher.register((Commands.literal("broadcast")
+          .requires((commandSourceStack) -> {
+            return commandSourceStack.hasPermission(2);
+          })).then(Commands.argument("targets", EntityArgument.players())
+              .then(Commands.argument("message", ComponentArgument.textComponent(registryAccess))
+                  .executes((commandContext) -> {
+                    int i = 0;
 
-                                        for (Iterator var2 = EntityArgument.getPlayers(commandContext, "targets")
-                                                .iterator(); var2.hasNext(); ++i) {
-                                            ServerPlayer serverPlayer = (ServerPlayer) var2.next();
-                                            BroadcastMessage(serverPlayer, ComponentUtils.updateForEntity(
-                                                    (CommandSourceStack) commandContext.getSource(),
-                                                    ComponentArgument.getComponent(commandContext, "message"),
-                                                    serverPlayer, 0));
-                                        }
-                                        return i;
-                                    }))));
-        });
-    }
+                    for (Iterator var2 = EntityArgument.getPlayers(commandContext, "targets")
+                        .iterator(); var2.hasNext(); ++i) {
+                      ServerPlayer serverPlayer = (ServerPlayer) var2.next();
+                      BroadcastMessage(serverPlayer, ComponentUtils.updateForEntity(
+                          (CommandSourceStack) commandContext.getSource(),
+                          ComponentArgument.getComponent(commandContext, "message"),
+                          serverPlayer, 0));
+                    }
+                    return i;
+                  }))));
+    });
+  }
 
-    public static void BroadcastMessage(ServerPlayer serverPlayer, Component message) {
-        org.agmas.noellesroles.packet.BroadcastMessageS2CPacket packet = new org.agmas.noellesroles.packet.BroadcastMessageS2CPacket(
-                message);
-        net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
-                .send(serverPlayer, packet);
-    }
+  public static void BroadcastMessage(ServerPlayer serverPlayer, Component message) {
+    org.agmas.noellesroles.packet.BroadcastMessageS2CPacket packet = new org.agmas.noellesroles.packet.BroadcastMessageS2CPacket(
+        message);
+    net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+        .send(serverPlayer, packet);
+  }
 }
