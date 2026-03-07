19e65d5e9fda255105810ebf3ddbcefa7e29f846
wifi-left <ysy1679682327@outlook.com>
2026-03-07 19:34:10 +0800

    调整

--- src/main/java/org/agmas/noellesroles/init/InitModRolesMax.java
+++ src/main/java/org/agmas/noellesroles/init/InitModRolesMax.java
@@ -176,7 +176,7 @@ public class InitModRolesMax {
         Harpymodloader.setRoleMaximum(ModRoles.SHERIFF_ID, NoellesRolesConfig.HANDLER.instance().sheriffMax);
         Harpymodloader.setRoleMaximum(ModRoles.BOMBER_ID, 1);
         Harpymodloader.setRoleMaximum(ModRoles.OLDMAN_ID, 1);
-        Harpymodloader.setRoleMaximum(ModRoles.JOJO_ID, 1);
+        Harpymodloader.setRoleMaximum(ModRoles.JOJO_ID, 0);
         Harpymodloader.setRoleMaximum(ModRoles.CHEF_ID, 1);
         Harpymodloader.setRoleMaximum(ModRoles.FORTUNETELLER_ID, 1);
         Harpymodloader.setRoleMaximum(ModRoles.WIND_YAOSE_ID, 1);
