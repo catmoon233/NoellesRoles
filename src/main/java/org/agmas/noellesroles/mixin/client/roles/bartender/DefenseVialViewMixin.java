a12a47652fba8742be0cee4ac25f429ae6a5f322
wifi-left <ysy1679682327@outlook.com>
2026-03-09 20:49:03 +0800

    FIX POISON VIEW

--- src/main/java/org/agmas/noellesroles/mixin/client/roles/bartender/DefenseVialViewMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/client/roles/bartender/DefenseVialViewMixin.java
@@ -2,6 +2,7 @@ package org.agmas.noellesroles.mixin.client.roles.bartender;
 
 import dev.doctor4t.trainmurdermystery.api.Role;
 import dev.doctor4t.trainmurdermystery.block_entity.BeveragePlateBlockEntity;
+import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
 import dev.doctor4t.trainmurdermystery.client.TMMClient;
 import dev.doctor4t.trainmurdermystery.game.GameFunctions;
 import dev.doctor4t.trainmurdermystery.index.TMMParticles;
@@ -32,12 +33,13 @@ public class DefenseVialViewMixin {
                 canSeePoison = role.identifier().getPath().equals(ModRoles.BARTENDER.identifier().getPath())
                         || role.identifier().getPath().equals(ModRoles.POISONER.identifier().getPath());
                 if (!canSeePoison) {
-                    if (world.players().stream().anyMatch((p) -> {
-                        return GameFunctions.isPlayerAliveAndSurvival(p)
-                                && TMMClient.gameComponent.isRole(p, ModRoles.POISONER);
-                    })) {
-                        canSeePoison = true;
-                    }
+                    if (GameWorldComponent.isKillerTeamRoleStatic(role))
+                        if (world.players().stream().anyMatch((p) -> {
+                            return GameFunctions.isPlayerAliveAndSurvival(p)
+                                    && TMMClient.gameComponent.isRole(p, ModRoles.POISONER);
+                        })) {
+                            canSeePoison = true;
+                        }
                 }
                 if (canSeePoison) {
                     world.addParticle(TMMParticles.POISON, true, (double) ((float) pos.getX() + 0.5F),
